287ced11319b8ae3ac3a5b4d6f9d74a318499ee5
AhmedRabieG <ahmedrabie010@gmail.com>
2025-02-18 17:47:22 +0200

    edit structure files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import './assets/styles/style.scss'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
@@ -7,23 +8,9 @@ import './stores/counter'
 import './stores/user'
 import 'font-awesome/css/font-awesome.min.css';
 
-import { Form, Field, ErrorMessage, defineRule } from 'vee-validate'
-import { required, email, min } from '@vee-validate/rules'
-
 
 const app = createApp(App)
-
 app.use(router)
 app.use(createPinia())
-
-app.component('Form', Form)
-app.component('Field', Field)
-app.component('ErrorMessage', ErrorMessage)
-
-// Define validation rules
-defineRule('required', required)
-defineRule('email', email)
-defineRule('min', min)
-
 
 app.mount('#app')
